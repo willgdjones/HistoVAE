9080af826e0eacfed347e2913ac1eec983b148da
William Jones <williamgdjones@gmail.com>
2018-08-12 22:03:51 +0100

    add seperate log dir for each run

--- src/models.py
+++ src/models.py
@@ -130,7 +130,7 @@ class ConvolutionalAutoencoder():
             callbacks=[
                 TensorBoard(
                     log_dir=(
-                        './tensorboardlogs/{}'
+                        './tensorboardlogs/{self.name}'
                     )
                 ),
             ],
