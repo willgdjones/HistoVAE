7bb2d679c9fa46e0e5da61a8a441deb09d41fcf6
William Jones <williamgdjones@gmail.com>
2018-08-23 12:18:26 +0100

    Use matplotlib Agg backend

--- scripts/view.py
+++ scripts/view.py
@@ -5,14 +5,17 @@ import click
 import logging
 import numpy as np
 from keras.models import load_model
+import matplotlib
 import matplotlib.pyplot as plt
 from os.path import isfile
 requests.packages.urllib3.disable_warnings()
 sys.path.append('.')
 from src.classes import Dataset, deprocess
 
+matplotlib.use('Agg')
 logger = logging.getLogger(__name__)
 MODEL_PATH = 'models/'
+
 
 @click.command()
 @click.option(
