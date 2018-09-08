d378097957fe4bf362a74f4dd0875c535161a504
William Jones <williamgdjones@gmail.com>
2018-09-08 17:54:56 +0100

    use mse instead of bce

--- src/models.py
+++ src/models.py
@@ -269,36 +269,6 @@ class VariationalConvolutionalAutoencoder(object):
 
         input_img_decoded = self.decode(self.encode(input_img))
 
-        # class CustomVariationalLayer(Layer):
-        #     def __init__(self, z_mean, z_log_var, patch_size, **kwargs):
-        #         self.is_placeholder = True
-        #         self.z_mean = z_mean
-        #         self.z_log_var = z_log_var
-        #         self.patch_size = patch_size
-        #         super(CustomVariationalLayer, self).__init__(**kwargs)
-        #
-        #     def vae_loss(self, x, x_decoded_mean_squash):
-        #         x = K.flatten(x)
-        #         x_decoded_mean_squash = K.flatten(x_decoded_mean_squash)
-        #         xent_loss = metrics.binary_crossentropy(
-        #             x, x_decoded_mean_squash
-        #         ) * self.patch_size**2
-        #         kl_loss = - 0.5 * K.mean(1 + self.z_log_var
-        #                                  - K.square(self.z_mean)
-        #                                  - K.exp(self.z_log_var), axis=-1)
-        #         return K.mean(xent_loss + kl_loss)
-        #
-        #     def call(self, inputs):
-        #         x = inputs[0]
-        #         x_decoded_mean_squash = inputs[1]
-        #         loss = self.vae_loss(x, x_decoded_mean_squash)
-        #         self.add_loss(loss, inputs=inputs)
-        #         # We don't use this output.
-        #         return x
-
-        # y = CustomVariationalLayer(
-        #     self.z_mean, self.z_log_var, self.params['patch_size']
-        #     )([input_img, input_img_decoded])
         model = Model(input_img, input_img_decoded)
         self.model = model
         return model
@@ -318,7 +288,7 @@ class VariationalConvolutionalAutoencoder(object):
         def vae_loss(x, x_decoded_mean_squash):
             x = K.flatten(x)
             x_decoded_mean_squash = K.flatten(x_decoded_mean_squash)
-            xent_loss = metrics.binary_crossentropy(
+            xent_loss = metrics.mean_squared_error(
                 x, x_decoded_mean_squash
             )
             kl_loss = - 0.5 * K.mean(1 + self.z_log_var
